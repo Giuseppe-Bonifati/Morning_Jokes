bc8db3e8d11267404aa999f017c5492240ef8815
Giuseppe Bonifati <110894389+Giuseppe-Bonifati@users.noreply.github.com>
2023-02-22 14:44:04 +0100

    Update joke.py

--- joke.py
+++ joke.py
@@ -4,6 +4,7 @@ from hidden import db_connection
 import smtplib
 
 
+# connection with the database
 db_connection = db_connection()
 
 conn = psycopg2.connect(
@@ -17,6 +18,7 @@ conn = psycopg2.connect(
 cursor = conn.cursor()
 
 
+#function to get form the api a random joke
 def funny_joke():
     '''return a string representing the random jokes from the API  https://official-joke-api.appspot.com/random_joke'''
     joke_request = requests.get("https://official-joke-api.appspot.com/random_joke")
@@ -29,15 +31,17 @@ def funny_joke():
 # test_email = ""
 # test_password = ""
 
-
+# get the email and the name of every user form the db
 cursor.execute("SELECT email, user_name FROM users")
 list_of_email = cursor.fetchall()
 conn.commit()
 
+# with SMTP send an email to every user
 with smtplib.SMTP("smtp.gmail.com",587, timeout=120) as connection:
         connection.starttls()
         connection.login(user=test_email, password=test_password)
-
+        
+        # list of email with get previously with fetchall (return a tuble of the every row ) 
         for i in list_of_email:
             connection.sendmail(
                 from_addr=test_email,
