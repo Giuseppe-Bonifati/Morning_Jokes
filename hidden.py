d0587d6a01efcb96dca102e95dc9eefb6a94d3d6
Giuseppe Bonifati <110894389+Giuseppe-Bonifati@users.noreply.github.com>
2023-02-22 14:37:36 +0100

    Update hidden.py

--- hidden.py
+++ hidden.py
@@ -1,4 +1,4 @@
-# This function will be imported to other files and contain the information of the database
+# This function contain the information of the database
 def db_connection():
     """Return the database information"""
     return {"database": "", "host": "", "user": "", "password": "", "port": ""}
